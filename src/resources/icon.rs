67cb8fb50d1fda592a6fd8aecb907e42d35c3894
Sergio Ribera <sergioalejandroriberacosta@gmail.com>
2023-02-20 03:09:16 -0400

    feat: impl clone for icon

--- src/resources/icon.rs
+++ src/resources/icon.rs
@@ -1,31 +1,39 @@
 use anyhow::Result;
-use eframe::{egui::{Context, Ui, Widget}, epaint::TextureId};
+use eframe::{
+    egui::{Context, Ui, Widget},
+    epaint::TextureId,
+};
 use egui_extras::image::FitTo;
 
 pub struct Icon {
     pub texture: egui_extras::RetainedImage,
     pub size: FitTo,
+    name: String,
+    bytes: Vec<u8>,
+    is_svg: bool,
 }
 
 impl Icon {
-    pub fn from_svg(name: &str, size: FitTo, alloc: &Context) -> Result<Self> {
+    pub fn from_svg(name: &str, size: FitTo) -> Result<Self> {
+        let bytes = std::fs::read(format!("./assets/icons/{name}"))?;
         Ok(Self {
             size,
-            texture: egui_extras::RetainedImage::from_svg_bytes_with_size(
-                name,
-                &std::fs::read(format!("./assets/icons/{name}"))?,
-                size,
-            ).unwrap(),
+            is_svg: true,
+            bytes: bytes.clone(),
+            name: name.to_string(),
+            texture: egui_extras::RetainedImage::from_svg_bytes_with_size(name, &bytes, size)
+                .unwrap(),
         })
     }
 
-    pub fn from_image(name: &str, size: FitTo, alloc: &Context) -> Result<Self> {
+    pub fn from_image(name: &str, size: FitTo) -> Result<Self> {
+        let bytes = std::fs::read(format!("./assets/{name}"))?;
         Ok(Self {
             size,
-            texture: egui_extras::RetainedImage::from_image_bytes(
-                name,
-                &std::fs::read(format!("./assets/{name}"))?,
-            ).unwrap(),
+            is_svg: false,
+            bytes: bytes.clone(),
+            name: name.to_string(),
+            texture: egui_extras::RetainedImage::from_image_bytes(name, &bytes).unwrap(),
         })
     }
 
@@ -39,6 +47,27 @@ impl Icon {
 
     pub fn show(&self, ui: &mut Ui) {
         self.texture.show(ui);
+    }
+}
+
+impl Clone for Icon {
+    fn clone(&self) -> Self {
+        Self {
+            texture: if self.is_svg {
+                egui_extras::RetainedImage::from_svg_bytes_with_size(
+                    &self.name,
+                    &self.bytes,
+                    self.size.clone(),
+                )
+                .unwrap()
+            } else {
+                egui_extras::RetainedImage::from_image_bytes(&self.name, &self.bytes).unwrap()
+            },
+            name: self.name.clone(),
+            is_svg: self.is_svg,
+            bytes: self.bytes.clone(),
+            size: self.size.clone(),
+        }
     }
 }
 
