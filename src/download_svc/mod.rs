0024ed8e84913acbc9a355cb0da58023a4320c1e
Sergio Ribera <sergioalejandroriberacosta@gmail.com>
2023-02-20 03:08:07 -0400

    feat: create download srv and default reusable download progress tracker

---
+++ src/download_svc/mod.rs
@@ -0,0 +1,66 @@
+mod icons;
+
+use std::sync::mpsc::{Receiver, SyncSender};
+
+pub use icons::*;
+use log::info;
+use mc_downloader::prelude::Reporter;
+
+#[derive(Clone, Copy, PartialEq)]
+pub enum DownloadProgressMessage {
+    Setup(u64),
+    Update(u64, u64),
+    End,
+}
+
+#[derive(Clone)]
+pub struct DownloadProgress {
+    curr_progress: u64,
+    max_progress: u64,
+    sender: SyncSender<DownloadProgressMessage>,
+}
+
+impl DownloadProgress {
+    pub fn new() -> (Self, Receiver<DownloadProgressMessage>) {
+        let (sender, recv) = std::sync::mpsc::sync_channel::<DownloadProgressMessage>(1);
+        (
+            Self {
+                curr_progress: 0,
+                max_progress: 0,
+                sender,
+            },
+            recv,
+        )
+    }
+}
+
+impl Reporter for DownloadProgress {
+    fn setup(&mut self, max_progress: u64) {
+        info!("Setup Reporter: {max_progress}");
+        self.max_progress = max_progress;
+        self.sender
+            .send(DownloadProgressMessage::Setup(max_progress))
+            .unwrap();
+    }
+
+    fn progress(&mut self, current: u64) {
+        info!(
+            "Setup progress\nCurr: {current}\nMax: {}",
+            self.max_progress
+        );
+        if current > 0 {
+            self.curr_progress += current;
+            self.sender
+                .send(DownloadProgressMessage::Update(
+                    self.curr_progress,
+                    self.max_progress,
+                ))
+                .unwrap();
+        }
+    }
+
+    fn done(&mut self) {
+        info!("Done progress");
+        self.sender.send(DownloadProgressMessage::End).unwrap();
+    }
+}
