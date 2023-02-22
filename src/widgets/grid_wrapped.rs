5354f9651ce5e26a7405f615ba3dfbdf43f6fe9c
Sergio Ribera <sergioalejandroriberacosta@gmail.com>
2023-02-22 09:46:16 -0400

    fix: flow of on_change callback and default button

--- src/widgets/grid_wrapped.rs
+++ src/widgets/grid_wrapped.rs
@@ -1,7 +1,8 @@
 use egui::{Color32, RichText, Sense, Stroke, Ui, Vec2};
 
-#[derive(Default)]
+#[derive(Default, Clone, Copy)]
 pub struct GridWrapped {
+    sended: bool,
     selected: Option<usize>,
 }
 
@@ -10,32 +11,34 @@ impl GridWrapped {
     pub fn show(
         &mut self,
         ui: &mut Ui,
-        btn_str: &str,
+        btn_str: Option<&str>,
         cell_size: impl Into<Vec2> + Clone,
         items: usize,
         draw_item: impl FnOnce(&mut Ui, usize) + Copy,
-        on_btn_click: impl FnOnce() + Copy,
-        on_change: impl FnOnce(usize) + Copy,
+        on_btn_click: impl FnOnce(),
+        on_change: impl FnOnce(usize),
     ) {
         egui::ScrollArea::vertical()
             .min_scrolled_width(ui.available_width())
             .min_scrolled_height(ui.available_height())
             .show(ui, |ui| {
                 ui.horizontal_wrapped(|ui| {
-                    let (rect, _resp) =
-                        ui.allocate_at_least(cell_size.clone().into(), Sense::click());
+                    if let Some(btn_str) = btn_str {
+                        let (rect, _resp) =
+                            ui.allocate_at_least(cell_size.clone().into(), Sense::click());
 
-                    ui.allocate_ui_at_rect(rect, |ui| {
-                        let btn = ui.add(
-                            eframe::egui::Button::new(RichText::new(btn_str).size(20.))
-                                .wrap(true)
-                                .min_size(egui::Vec2::new(70., 70.)),
-                        );
-                        if btn.clicked() {
-                            on_btn_click();
-                        }
-                        ui.add_space(5.);
-                    });
+                        ui.allocate_ui_at_rect(rect, |ui| {
+                            let btn = ui.add(
+                                eframe::egui::Button::new(RichText::new(btn_str).size(20.))
+                                    .wrap(true)
+                                    .min_size(cell_size.clone().into()),
+                            );
+                            if btn.clicked() {
+                                on_btn_click();
+                            }
+                            ui.add_space(5.);
+                        });
+                    }
 
                     for i in 0..items {
                         let (rect, resp) =
@@ -67,10 +70,16 @@ impl GridWrapped {
                         });
                         if resp.clicked() {
                             self.selected = Some(i);
-                            on_change(i);
+                            self.sended = false;
                         }
                     }
                 });
             });
+        if let Some(i) = self.selected {
+            if !self.sended {
+                on_change(i);
+                self.sended = true;
+            }
+        }
     }
 }
