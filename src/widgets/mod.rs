afd67b3c5f9e5a4d669db9b6faf11e13983e04a6
Sergio Ribera <sergioalejandroriberacosta@gmail.com>
2023-03-07 23:49:56 -0400

    feat: add new widgets and calculate ratio

--- src/widgets/mod.rs
+++ src/widgets/mod.rs
@@ -1,6 +1,9 @@
+mod central_panel;
 mod covered_image;
 mod grid_wrapped;
 mod icon_button;
+mod image_button;
+mod modal;
 mod progress_button;
 mod steps;
 mod tabs;
@@ -8,13 +11,17 @@ mod title_bar;
 mod toast;
 
 use crate::data::APP_INFO;
+use egui::Vec2;
 use egui_stylist::StylistFileDialog;
 use log::debug;
 use std::path::PathBuf;
 
+pub use central_panel::*;
 pub use covered_image::*;
 pub use grid_wrapped::*;
 pub use icon_button::IconButton;
+pub use image_button::*;
+pub use modal::*;
 pub use progress_button::*;
 pub use steps::*;
 pub use tabs::*;
@@ -49,4 +56,26 @@ pub fn open_file_dialog(
             builder.set_directory(&theme_folder).save_file()
         }
     }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub enum CoveredRatioType {
+    Cover,
+    Container,
+}
+
+pub fn calculate_ratio_size(
+    object_size: Vec2,
+    container_size: Vec2,
+    ratio_type: CoveredRatioType,
+) -> Vec2 {
+    let w_ratio = container_size.x / object_size.x;
+    let h_ratio = container_size.y / object_size.y;
+
+    let ratio = match ratio_type {
+        CoveredRatioType::Cover => w_ratio.max(h_ratio),
+        CoveredRatioType::Container => w_ratio.min(h_ratio),
+    };
+
+    Vec2::new(object_size.x * ratio, object_size.y * ratio)
 }
