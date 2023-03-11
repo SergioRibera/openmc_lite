d353e183940f0e3b222b3e1cca029e54afb2cff5
Sergio Ribera <sergioalejandroriberacosta@gmail.com>
2023-03-10 21:28:21 -0400

    fix: selectable instance at home and java path variable

--- src/screens/home.rs
+++ src/screens/home.rs
@@ -1,17 +1,15 @@
 use eframe::egui::{ComboBox, Layout, Ui};
-use log::info;
 
 use crate::{
     resources::ResourceLoader,
+    screens::utils::launch_instance,
     settings::LauncherSettings,
     widgets::{CoveredImage, CoveredRatioType, ImageButton},
 };
 
 #[inline]
-pub fn home(ui: &mut Ui, conf: &LauncherSettings, res: &ResourceLoader) {
-    // if !ui.is_rect_visible(ui.max_rect()) {
-    //     return;
-    // }
+pub fn home(ui: &mut Ui, conf: &mut LauncherSettings, res: &ResourceLoader) {
+    let exists_instance = conf.last_launched.is_some();
     let mut value = conf
         .last_launched
         .clone()
@@ -24,11 +22,12 @@ pub fn home(ui: &mut Ui, conf: &LauncherSettings, res: &ResourceLoader) {
         ui.add_space(10.);
         ui.with_layout(Layout::right_to_left(eframe::emath::Align::Max), |ui| {
             ui.add_space(10.);
+            ui.set_enabled(exists_instance);
             ComboBox::from_id_source("Select Version")
                 .selected_text(value.as_str())
                 .show_ui(ui, |ui| {
                     ui.style_mut().wrap = Some(false);
-                    ui.set_min_width(60.0);
+                    ui.set_min_width(ui.available_width());
                     conf.instances.iter().for_each(|i| {
                         ui.selectable_value(&mut value, i.name.clone(), i.name.clone());
                     })
@@ -38,8 +37,18 @@ pub fn home(ui: &mut Ui, conf: &LauncherSettings, res: &ResourceLoader) {
             !value.is_empty(),
             ImageButton::new(res.btn_bg.texture_id(ui.ctx()), (250., 80.)),
         );
-        if btn_play.clicked() {
-            info!("Btn Clicked");
+
+        if let Some(ins) = conf.last_launched.clone() {
+            let instance = if value != ins.name {
+                let instance = conf.instances.iter().find(|f| f.name == value).unwrap();
+                conf.last_launched.replace(instance.clone());
+                instance
+            } else {
+                &ins
+            };
+            if btn_play.clicked() {
+                launch_instance(instance, conf);
+            }
         }
     });
 }
